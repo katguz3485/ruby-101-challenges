7fa153424bb2820267107fcfde5df9826723e411
katguz3485 <katarzyna.guzik01@gmail.com>
2018-12-09 20:24:03 +0100

    [Add, Remove] some tasks solutions, .idea to gitignore and rm

--- 12-filter_list/lib/filter_listt.rb
+++ 12-filter_list/lib/filter_listt.rb
@@ -1,14 +1,13 @@
-
 def filter_list(l)
- # words.split().sort_by {|w| w.chars.min }.join(" ")
- # words.split().sort_by {|w| w[/[^\d]/]}.join(" ")
-  words.split(' ').sort_by {|s| s.gsub(/\D/, '').to_i}.join(' ')
+
+
+  l.select {|w| w.is_a? Numeric}
+  # l.delete_if { |x| x.class == String }
 
 end
 
 
-
-p filter_list("is2 Thi1s T4est 3a")
+p filter_list([1, 2, 'a', 'b'])
 
 
 
--- 12-filter_list/spec/filter_list_spec.rb
+++ 12-filter_list/spec/filter_list_spec.rb
@@ -1,12 +1,13 @@
-require_relative '../lib/your_order'
+require_relative '../lib/filter_listt'
 
-describe '#your_order' do
+describe '#filter_list' do
 
 
-  it 'should sort words in by number that is included in this word' do
-    expect(order("is2 Thi1s T4est 3a")).to eq("Thi1s is2 3a T4est")
-    expect(order("4of Fo1r pe6ople g3ood th5e the2")).to eq("Fo1r the2 g3ood 4of th5e pe6ople")
-    expect(order("")).to eq("")
+  it 'filter' do
+    expect(filter_list([1, 2, 'a', 'b'])).to eq([1, 2])
+    expect(filter_list([1, 2, 'aasf', '1', '123', 123])).to eq([1, 2, 123])
+
+
   end
 
 

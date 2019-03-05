e1ed18924de03bf29d7f266dd5ed153f93b65747
katguz3485 <katarzyna.guzik01@gmail.com>
2019-03-06 00:02:45 +0100

    code wars tasks

--- 27-ruby_train/the_same_with_regex.rb
+++ 27-ruby_train/the_same_with_regex.rb
@@ -1,3 +1,19 @@
 class TheSameWithRegex
 
+
+  def valid_parentheses(string)
+
+    a = string.gsub(/[^\(\)]/, '')
+    while a.gsub!('()', '');
+    end
+    a.empty?
+
+  end
+
+
+
 end
+
+validator = TheSameWithRegex.new
+
+pp validator.valid_parentheses("((()")

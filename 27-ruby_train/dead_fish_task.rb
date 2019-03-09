90bc8bdc58990aad1ca3e483c0ad43dec5499b06
katguz3485 <katarzyna.guzik01@gmail.com>
2019-03-09 19:43:58 +0100

    rpn and dead_fish task

---
+++ 27-ruby_train/dead_fish_task.rb
@@ -0,0 +1,33 @@
+require 'rails'
+require 'pp'
+require 'pry'
+#Deadfish has 4 commands, each 1 character long:
+
+# i increments the value (initially 0)
+# d decrements the value
+# s squares the value
+# o outputs the value into the return array
+# Invalid characters should be ignored.
+#
+class DeadFish
+
+  def parse(text)
+    output = []
+    counter = 0
+
+    text.each_char do |char|
+      counter += 1 if char == "i"
+      counter -= 1 if char == "d"
+      counter **= 2 if char == "s"
+      output << counter if char == "o"
+    end
+    output
+  end
+
+end
+
+
+df = DeadFish.new
+pp df.parse("iiisdoso")
+
+# parse("iiisdoso")  ==>  [8, 64]

2ccf67f0d2e575b303f93ffada14550376ca853b
katguz3485 <katarzyna.guzik01@gmail.com>
2019-03-10 18:34:05 +0100

    service refact

---
+++ 20-pub_chem_api_consumed/lib/pub_chem_service_again.rb
@@ -0,0 +1,44 @@
+require 'pp'
+require 'httparty'
+require 'json'
+require 'rails'
+require 'uri'
+require 'pry-rails'
+
+class PubChemServiceAgain
+  include HTTParty
+
+  base_uri 'https://pubchem.ncbi.nlm.nih.gov/rest/pug'
+
+  attr_accessor :property, :cas
+
+  def initialize(cas:)
+    @property = property
+    @cas = cas
+
+  end
+
+  def call
+    cas_to_cid
+  end
+
+  private
+
+  def cas_to_cid
+    response = self.class.get("/compound/name/#{cas}/cids/JSON")
+    if response.success?
+      result = response["IdentifierList"]["CID"]
+    else
+      raise response.response
+    end
+    cid = result.join("")
+  end
+end
+
+puts pubChem = PubChemServiceAgain.new(cas: "50-78-2").call
+
+# puts pubChem.to_hash_object
+# puts pubChem.show_property("MolecularWeight")
+# puts pubChem.picture(cid)
+
+
---
+++ 27-ruby_train/reverse_polish_notation.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+# For example expression 5 1 2 + 4 * + 3 - (which is equivalent to 5 + ((1 + 2) * 4) - 3 in normal notation) should evaluate to 14.
+#
+# For your convenience, the input is formatted such that a space is provided between every token.
+#
+# Empty expression should evaluate to 0.
+#
+# Valid operations are +, -, *, /.
+#
+# You may assume that there won't be exceptional situations (like stack underflow or division by zero).
+
+
+class ReversePolishCalc
+
+  OPERATORS = %w(+ - * / )
+
+  def calc(expr)
+
+    return 0 if expr == ""
+
+    arr = expr.split
+    stack = []
+
+    arr.each do |elem|
+      if OPERATORS.include?(elem)
+        stack << stack.pop(2).reduce(elem)
+      else
+        stack << elem.to_f
+        stack.last
+      end
+    end
+    stack.last
+  end
+
+end
+
+
+rp = ReversePolishCalc.new
+puts rp.calc("1 3 -")
+puts rp.calc("4 2 /")
+puts rp.calc("1 3 *")
+
